14d27cce4614c3e290b39ff8e6a5ed9d6656cb16
Sam Kad <samkad@Sams-MacBook-Pro.local>
2018-10-01 13:58:45 +0300

    Testing logo image 1

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -19,7 +19,7 @@
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container">
             <a class="navbar-brand" href="index.html">
-                <img src="{{ URL::asset('citylab/img/logo.png') }}" alt="Girl in a jacket">
+                <img src="{{ asset('citylab/img/logo.png') }}" alt="Girl in a jacket">
 
             </a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample05" aria-controls="navbarsExample05" aria-expanded="false" aria-label="Toggle navigation">
